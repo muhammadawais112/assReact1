6a7bc759c6a3859ddb71cb2139a24951ea922ebb
muhammadawais112 <simpleboy1917@gmail.com>
2022-02-17 14:45:51 +0500

    assignment

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import './App.css';
+
+function App() {
+  return (
+    <div className="container ">
+      <div id="header" className="row">
+        <div className="col-md-4 m-5">
+          <h1>Title of a longer  featured blog post</h1>
+
+          {/* <p  id="header"> Title of a longer  featured blog post </p> */}
+
+          <p>Multiple lines of text that form the data informing reader quickly and efficently about what`s most interesting in this post`s content</p>
+          <h5>Continue reading...</h5>
+        </div>
+
+
+
+      </div>
+
+      <div className="container">
+        <div className="row mt-3 ml-5">
+          <div className="cols col-md-9  ">
+            <h5>From the Firehose</h5>
+            <hr />
+            <h2>Sample blog post</h2>
+            <p>January 1,2014 by <a href="">Mark</a></p>
+
+            <p> Tempore eius quae consequuntur eligendi, dignissimos qui praesentium omnis amet facere nisi harum, quibusdam iste et neque! Hic voluptatibus consequuntur, ab recusandae earum expedita. Ad nisi nihil, natus vero fugit rem perferendis, iusto eum facere velit ut ex repellat corrupti repudiandae magnam unde harum obcaecati! Nam, ipsa totam. Eos esse quasi ullam.</p>
+            <hr />
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas inventore, distinctio aspernatur molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <p> Quas inventore, distinctio aspernatur molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <p>  molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <h2>Heading</h2>
+            <p>  molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+            <h2>Sub-Heading</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ex?</p>
+            <p>Lorem, ipsum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloremque iure libero, suscipit rem fuga!</p>
+
+            <h2>Sub-Heading</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus reiciendis ab adipisci repudiandae perferendis sint nihil unde illum, quae minus recusandae asperiores facere ut tenetur voluptatem, et eaque maxime iusto?</p>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <p className="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, et?</p>
+
+            <ol>1.Lorem ipsum dolor sit amet.</ol>
+            <ol>2.Lorem ipsum dolor sit amet.</ol>
+            <ol>3.Lorem ipsum dolor sit, amet consectetur adipisicing elit.</ol>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet.</p>
+            <h2>Another blog post</h2>
+            <p>January 1,2014 by <a href="">Mark</a></p>
+
+            <p> Tempore eius quae consequuntur eligendi, dignissimos qui praesentium omnis amet facere nisi harum, quibusdam iste et neque! Hic voluptatibus consequuntur, ab recusandae earum expedita. Ad nisi nihil, natus vero fugit rem perferendis, iusto eum facere velit ut ex repellat corrupti repudiandae magnam unde harum obcaecati! Nam, ipsa totam. Eos esse quasi ullam.</p>
+
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas inventore, distinctio aspernatur molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <p> Quas inventore, distinctio aspernatur molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <p>  molestiae aliquid minus, laborum consequatur porro voluptatibus excepturi doloremque dolorum maiores est exercitationem voluptatum cum ullam ipsa voluptates.
+            </p>
+
+            <h2>New Features</h2>
+            <p>January 1,2014 by <a href="">Chirs</a></p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus reiciendis ab adipisci repudiandae perferendis sint nihil unde illum, quae minus recusandae asperiores facere ut tenetur voluptatem, et eaque maxime iusto?</p>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam distinctio repudiandae.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+
+
+          </div>
+          <div className="cols col-md-3 ml-5">
+            <h5><b><i> About</i></b> </h5>
+            <div class="card w-100">
+              <div class="card-body">
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+              </div>
+
+            </div>
+
+            
+            <div>
+              <h3><i>Archives</i></h3>
+
+              <a className='anker' href="">March 2014</a>
+              <a className='anker' href="">February 2014</a>
+              <a className='anker' href="">January 2014</a>
+              <a className='anker' href="">December 2013</a>
+              <a className='anker' href="">November 2013</a>
+              <a className='anker' href="">October 2013</a>
+              <a className='anker' href="">September 2013</a>
+              <a className='anker' href="">August 2013</a>
+              <a className='anker' href="">July 2013</a>
+              <a className='anker' href="">June 2013</a>
+              <a className='anker' href="">May 2013</a>
+            </div>
+
+            <h3 className="mt-2"><i>ElseWhere</i></h3>
+            <a className='anker' href="">GitHub</a>
+            <a className='anker' href="">Twitter</a>
+            <a className='anker' href="">Facebook</a>
+
+
+          </div>
+        </div>
+      </div>
+
+      <button type="button" style={{ borderRadius: '20px', marginTop: '30px' }} className="btn btn-outline-primary">Older</button>
+      <button type="button" style={{ borderRadius: '20px', marginTop: '30px' }} className="btn btn-outline-primary">Older</button>
+
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
